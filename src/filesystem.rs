9e51f7863ad158ef2c96e7524efc26a061f393bb
neonew <neonew@gmail.com>
2024-01-04 00:08:54 +0100

    Drop most of HelloFS implementation

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,124 +1,29 @@
 use std::ffi::OsStr;
-use std::time::{Duration, UNIX_EPOCH};
+use std::time::Duration;
 
-use fuser::{FileAttr, Filesystem, FileType, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry, Request};
-use libc::{EIO, ENOENT};
+use fuser::{Filesystem, ReplyDirectory, ReplyEntry, Request};
+use libc::ENOSYS;
+use log::debug;
 
-const TTL: Duration = Duration::from_secs(1); // 1 second
-
-const HELLO_DIR_ATTR: FileAttr = FileAttr {
-    ino: 1,
-    size: 0,
-    blocks: 0,
-    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
-    mtime: UNIX_EPOCH,
-    ctime: UNIX_EPOCH,
-    crtime: UNIX_EPOCH,
-    kind: FileType::Directory,
-    perm: 0o755,
-    nlink: 2,
-    uid: 501,
-    gid: 20,
-    rdev: 0,
-    flags: 0,
-    blksize: 512,
-};
-
-const HELLO_TXT_CONTENT: &str = "Hello World!\n";
-
-const HELLO_TXT_ATTR: FileAttr = FileAttr {
-    ino: 2,
-    size: 13,
-    blocks: 1,
-    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
-    mtime: UNIX_EPOCH,
-    ctime: UNIX_EPOCH,
-    crtime: UNIX_EPOCH,
-    kind: FileType::RegularFile,
-    perm: 0o644,
-    nlink: 1,
-    uid: 501,
-    gid: 20,
-    rdev: 0,
-    flags: 0,
-    blksize: 512,
-};
+const _TTL: Duration = Duration::from_secs(1); // 1 second
 
 pub struct HelloFS;
 
 impl Filesystem for HelloFS {
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
-        if parent == 1 && name.to_str() == Some("hello.txt") {
-            reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
-        } else {
-            reply.error(ENOENT);
-        }
-    }
-
-    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
-        match ino {
-            1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
-            2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
-            _ => reply.error(ENOENT),
-        }
-    }
-
-    fn read(
-        &mut self,
-        _req: &Request,
-        ino: u64,
-        _fh: u64,
-        offset: i64,
-        _size: u32,
-        _flags: i32,
-        _lock: Option<u64>,
-        reply: ReplyData,
-    ) {
-        if ino == 2 {
-            let Ok(size) = usize::try_from(offset) else {
-                reply.error(EIO);
-                return;
-            };
-            reply.data(&HELLO_TXT_CONTENT.as_bytes()[size..]);
-        } else {
-            reply.error(ENOENT);
-        }
+        debug!("lookup(parent: {parent:#x?}, name: {name:?})");
+        reply.error(ENOSYS);
     }
 
     fn readdir(
         &mut self,
         _req: &Request,
         ino: u64,
-        _fh: u64,
+        fh: u64,
         offset: i64,
-        mut reply: ReplyDirectory,
+        reply: ReplyDirectory,
     ) {
-        if ino != 1 {
-            reply.error(ENOENT);
-            return;
-        }
-
-        let entries = vec![
-            (1, FileType::Directory, "."),
-            (1, FileType::Directory, ".."),
-            (2, FileType::RegularFile, "hello.txt"),
-        ];
-
-        let Ok(size) = usize::try_from(offset) else {
-            reply.error(EIO);
-            return;
-        };
-
-        for (i, entry) in entries.into_iter().enumerate().skip(size) {
-            // i + 1 means the index of the next entry
-            let Ok(next_entry) = i64::try_from(i + 1) else {
-                reply.error(EIO);
-                return;
-            };
-            if reply.add(entry.0, next_entry, entry.1, entry.2) {
-                break;
-            }
-        }
-        reply.ok();
+        debug!("readdir(ino: {ino:#x?}, fh: {fh}, offset: {offset})");
+        reply.error(ENOSYS);
     }
 }
--- tests/filesystem_test.rs
+++ tests/filesystem_test.rs
@@ -1,10 +1,11 @@
+use ErrorKind::Unsupported;
 use std::error::Error;
 use std::fs;
 use std::fs::create_dir;
+use std::io::ErrorKind;
 use std::path::Path;
 
 use fuser::MountOption::{FSName, RW};
-use pretty_assertions::assert_eq;
 
 use gpgfs_rust::filesystem::HelloFS;
 
@@ -19,13 +20,15 @@ fn test_list_directory_entries() -> Result<(), Box<dyn Error>> {
     let options = vec![RW, FSName("gpgfs-rust".to_string())];
     let session = fuser::spawn_mount2(HelloFS, &plain, &options)?;
 
-    assert_eq!(get_files(&plain)?, vec!["hello.txt"]);
+    let files = get_files(&plain);
+    assert_eq!(files.unwrap_err().kind(), Unsupported);
+    // assert_eq!(files?, Vec::<String>::new());
 
     session.join();
     Ok(())
 }
 
-fn get_files(path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
+fn get_files(path: &Path) -> Result<Vec<String>, std::io::Error> {
     Ok(fs::read_dir(path)?
         .filter_map(|entry| Some(entry.ok()?.path().file_name()?.to_str()?.to_string()))
         .collect::<Vec<_>>())
